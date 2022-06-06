5746ad603539e38dc23e943853dd45d04a282694
goldin2008 <goldin20082011@gmail.com>
2022-06-06 00:20:13 -0400

    Update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ App main file
 """
 # Put the code for your API here.
 import os
+import sys
 import yaml
 
 # Import Uvicorn & the necessary modules from FastAPI
@@ -21,7 +22,7 @@ import starter.ml.data
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
     if os.system("dvc pull") != 0:
-        exit("dvc pull failed")
+        sys.exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 with open('config.yml') as f:
